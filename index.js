fa581dced70360df967d7c25a0b5d94d67741d24
Bryan Bosire <bryanbosire@gmail.com>
2022-06-29 14:54:51 +0300

    commit tree 512262c5211a254e3ca977159758cf963cba5e0f

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
 /* Your Code Here */
+const createEmployeeRecord = employeeArr => {
+    const[firstName, familyName,title, payPerHour, timeInEvents,timeOutEvents] = employeeArr
+    const record = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return record
+}
+
+const createEmployeeRecords = employeeArr => {
+    return employeeArr.map(record => createEmployeeRecord(record))
+}
+
+const createTimeInEvent = function (dateTime) {
+    const [date, time] = dateTime.split(' ')
+    this.timeInEvents.push({
+        type : "TimeIn",
+        hour : parseInt(time),
+        date : date
+    })
+    return this
+}
+
+function createTimeOutEvent(dateTime) {
+    const [date, time] = dateTime.split(' ')
+    this.timeOutEvents.push({
+        type : "TimeOut",
+        hour : parseInt(time),
+        date : date
+    })
+    return this
+}
+
+const hoursWorkedOnDate = function (date) {
+    const timeIn = this.timeInEvents.find(e => e.date === date)
+    const timeOut = this.timeOutEvents.find(e => e.date === date)
+
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+const wagesEarnedOnDate = function (date) {
+    const hours = hoursWorkedOnDate.call(this, date)
+    return hours * this.payPerHour
+}
+
+const findEmployeeByFirstName = function (collection,firstName) {
+    return collection.find(e => e.firstName === firstName)
+}
+
+const calculatePayroll = (employeeArray) => {
+    return employeeArray.reduce(function (memo, e) {
+        return memo + allWagesFor.call(e)
+    }, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
